e5d3ae1a68c4dc31620e2577cc16422d198596cb
lausofdiaz <144862180+lausofdiaz@users.noreply.github.com>
2023-09-23 19:55:12 -0500

    calculadora
    operaciones

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,38 +3,43 @@ import React, { useState } from 'react';
 import "./estilos.css";
 export default function App() {
 
-  const [valor, setValor] = useState([]);
-  const handleClick = (val) =>{
-    setValor(val);
-  };
+  const [valor, setValor] = useState('');
+ 
+  const borrar = (v)=>{
+    setValor('')
+  }
+  
+  const calcular = ()=>{
+    setValor(eval(valor).toString());
+  }
 
   
   return (
     <section>
       <div  className="calculadora">
       <div className="pantalla">
-        {valor}
+      {valor}
       </div>
       <div className="numeros">
-      <button className="siete" onClick={() => handleClick('7')}>7</button>
-      <button className="ocho" onClick={() => handleClick('8')}>8</button>
-      <button className="nueve" onClick={() => handleClick('9')}>9</button>
-      <button className="cuatro" onClick={() => handleClick('4')}>4</button>
-      <button className="cinco" onClick={() => handleClick('5')}>5</button>
-      <button className="seis" onClick={() => handleClick('6')}>6</button>
-      <button className="uno" onClick={() => handleClick('1')}>1</button>
-      <button className="dos" onClick={() => handleClick('2')}>2</button>
-      <button className="tres" onClick={() => handleClick('3')}>3</button>
-      <button className="cero" onClick={() => handleClick('0')}>0</button>
+      <button className="siete" onClick={() => setValor(valor+'7')}>7</button>
+      <button className="ocho" onClick={() => setValor(valor+'8')}>8</button>
+      <button className="nueve" onClick={() => setValor(valor+'9')}>9</button>
+      <button className="cuatro" onClick={() => setValor(valor+'4')}>4</button>
+      <button className="cinco" onClick={() => setValor(valor+'5')}>5</button>
+      <button className="seis" onClick={() => setValor(valor+'6')}>6</button>
+      <button className="uno" onClick={() => setValor(valor+'1')}>1</button>
+      <button className="dos" onClick={() => setValor(valor+'2')}>2</button>
+      <button className="tres" onClick={() => setValor(valor+'3')}>3</button>
+      <button className="cero" onClick={() => setValor(valor+'0')}>0</button>
       </div>
       <div className="operacion">
-      <button className="c" onClick={() => handleClick('')}>C</button>
-      <button className="porcentaje" onClick={() => handleClick('%')}>%</button>
-      <button className="mas" onClick={() => handleClick('+')}>+</button>
-      <button className="menos" onClick={() => handleClick('-')}>-</button>
-      <button className="division" onClick={() => handleClick('/')}>/</button>
-      <button className="multiplicacion" onClick={() => handleClick('X')}>X</button>
-      <button className="igual" onClick={() => handleClick('=')}>=</button>
+      <button className="c" onClick={borrar}>C</button>
+      <button className="porcentaje" onClick={() => setValor(valor+'%')}>%</button>
+      <button className="mas" onClick={() => setValor(valor+'+')}>+</button>
+      <button className="menos" onClick={() => setValor(valor+'-')}>-</button>
+      <button className="division" onClick={() => setValor(valor+'/')}>/</button>
+      <button className="multiplicacion" onClick={() => setValor(valor+'*')}>X</button>
+      <button className="igual" onClick={calcular}>=</button>
       </div>
       </div>
     </section>
